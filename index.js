a1317e475278aac0b7442f0f540e9c50e94d2860
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2023-11-13 00:59:54 +0100

    add some controll for create a join rooms

--- index.js
+++ index.js
@@ -12,10 +12,31 @@ const server = app.listen(PORT, function () {
 // Static files
 app.use(express.static("public"));
 
+//Utils
+rooms = { 'prova': { player1: '', player2: '' } }
 // Socket setup
 const io = socket(server);
 
 io.on("connection", (socket) => {
     console.log("Made socket connection", socket.id);
 
+    socket.on("get rooms", (callback) => {
+        callback(getAvailableRooms())
+    })
+
+    socket.on("create room", (roomName, player1) => {
+        rooms[roomName] = { 'player1': player1, player2: '' }
+    })
+
 });
+
+
+const getAvailableRooms = () => {
+    var temp = {}
+    for (var key of Object.keys(rooms)) {
+        if (rooms[key].player2 == '') {
+            temp[key] = rooms[key]
+        }
+    }
+    return temp;
+}
--- public/main.js
+++ public/main.js
@@ -1,1 +1,32 @@
 const socket = io();
+
+//add to select the available rooms
+socket.emit("get rooms", (response) => {
+    const roomsList = document.getElementById("roomsList");
+    roomsList.innerHTML = '';
+    Object.keys(response).forEach(response => {
+        const option = document.createElement("option");
+        option.text = response;
+        roomsList.add(option);
+    });
+});
+//-------------------------------------------------------------
+
+
+const createRoom = () => {
+    if (document.getElementById("name").value != "") {
+        
+
+    } else {
+        alert("Insert name")
+    }
+}
+
+const joinRoom = () => {
+    if (document.getElementById("name").value != "") {
+
+
+    } else {
+        alert("Insert name")
+    }
+}
